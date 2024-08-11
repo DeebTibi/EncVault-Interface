ce0f7f3f1a04594ad90fa16e647dcdcb4d0a3768
deeb <dibtibi@mail.tau.ac.il>
2024-08-11 19:56:05 +0300

    signup button

--- app/landing/page.js
+++ app/landing/page.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import styles from './page.module.css'
-import { Inter } from 'next/font/google'
-import NextLink from 'next/link'
-import { Button, Link } from '@chakra-ui/react'
+import React from "react";
+import styles from "./page.module.css";
+import { Inter } from "next/font/google";
+import NextLink from "next/link";
+import { Button, Link } from "@chakra-ui/react";
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function LandingPage() {
   return (
@@ -13,22 +13,59 @@ export default function LandingPage() {
         <div className={styles.left_container}>
           <div className={styles.item_container}>
             <div className={styles.text_container}>
-              <h1 className={`${inter.className}`} style={{fontWeight: "600", fontSize: 50}}>Welcome to</h1>
-              <h1 className={`${inter.className} ${styles.hero_text}`} style={{fontWeight: "800", fontSize: 50}}>EncVault.</h1>
+              <h1
+                className={`${inter.className}`}
+                style={{ fontWeight: "600", fontSize: 50 }}
+              >
+                Welcome to
+              </h1>
+              <h1
+                className={`${inter.className} ${styles.hero_text}`}
+                style={{ fontWeight: "800", fontSize: 50 }}
+              >
+                EncVault.
+              </h1>
             </div>
-            <div className={styles.button_container}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: 10,
+                width: "100%",
+                height: "fit-content",
+              }}
+            >
               <NextLink href="/dashboard">
-                <Button colorScheme={"blue"} variant={"outline"} className={`${inter.className}`} fontWeight={"800"} size={"lg"}>Get Started</Button>
+                <Button
+                  colorScheme={"blue"}
+                  variant={"outline"}
+                  className={`${inter.className}`}
+                  fontWeight={"800"}
+                  size={"lg"}
+                >
+                  Get Started
+                </Button>
+              </NextLink>
+              <NextLink href="/signup">
+                <Button
+                  colorScheme={"blue"}
+                  variant={"outline"}
+                  className={`${inter.className}`}
+                  fontWeight={"800"}
+                  size={"lg"}
+                >
+                  Register
+                </Button>
               </NextLink>
             </div>
           </div>
         </div>
         <div className={styles.right_container}>
           <div className={styles.image_container}>
-          <img src="/undraw_secure_files_re_6vdh.png"/>
+            <img src="/undraw_secure_files_re_6vdh.png" />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
