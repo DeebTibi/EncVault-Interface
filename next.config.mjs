542a4a28eabafbd83901cabbc698057ab80d6ba5
deeb <dibtibi@mail.tau.ac.il>
2024-08-11 19:58:14 +0300

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/landing",
+        permanent: true,
+      },
+    ];
+  },
   experimental: {
     serverActions: {
       allowedOrigins: ["abualdeeb.com", "localhost:3000", "abualdeeb.com:8000"],
